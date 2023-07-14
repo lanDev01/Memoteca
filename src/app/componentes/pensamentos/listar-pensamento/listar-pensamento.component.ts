0bc3d24ea01e64c83af41c58ffce608c1483d114
Alan <lnjnr9215@gmail.com>
2023-07-14 18:07:02 -0300

    simulating backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamentos';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,21 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-  listaPensamento = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'É a única estrela do Sistema Solar, conjunto de corpos celestes (planetas, planetas-anões, asteroides, cometas, meteoritos) que gira em torno do Sol. Tem 333 mil vezes a massa da Terra, e suas dimensões são 110 vezes maiores do que as do nosso planeta. É composto essencialmente pelos gases hidrogênio e hélio.',
-      autoria: 'Componente filho',
-      modelo: 'modelo3'
-    }
-  ]
+  listaPensamento: Pensamento[] = [];
 }
