a54c929bee40f08c0d9534ab2cbb01a5c8485add
LanDev01 <alnjr0001@outlook.com>
2024-07-20 18:15:54 -0300

    style: ajustes no footer e header, estilizando mural de pensamentos

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PensamentoService } from '../pensamentos/pensamento.service';
+import { Pensamento } from '../pensamentos/pensamentos';
 
 @Component({
   selector: 'app-cabecalho',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./cabecalho.component.css']
 })
 export class CabecalhoComponent {
+  favoritos: boolean = false
+  paginaAtual: number = 1
 
+  titulo: string = 'Meu Mural'
+  haMaisPensamentos: boolean = true
+  filtro: string = ''
+  listaPensamentos: Pensamento[] = [];
+  listaFavoritos: Pensamento[] = []
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) {}
+
+  recarregarComponente() {
+    this.favoritos = false
+    this.paginaAtual = 1
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
+  }
+
+  listarFavoritos() {
+    this.titulo = 'Meus Favoritos'
+    this.favoritos = true
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe(listarPensamentosFavoritos => {
+        this.listaPensamentos = listarPensamentosFavoritos
+        this.listaFavoritos = listarPensamentosFavoritos
+      })
+  }
 }
